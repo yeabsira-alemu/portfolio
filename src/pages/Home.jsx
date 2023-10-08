196515ff4a4621b3a40a6e53ac2e1133c652258d
Yab <yeabalex18@gmail.com>
2023-10-08 10:28:07 -0700

    nice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-import "./Home.css"
-export default function HomePage(){
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import LandingPageImage from "../assets/image-removebg-preview.png";
 
-    return (
-        <div className="home-page-container">
-            <div className="home-page"></div>
+export default function HomePage() {
+  const [textChanger, setTextChanger] = useState(true);
+
+  useEffect(() => {
+    const intervalChanger = setInterval(() => {
+      setTextChanger((prev) => !prev);
+    }, 6000);
+
+    return () => {
+      clearInterval(intervalChanger);
+    };
+  }, [1]);
+
+  const animStyle = {
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    width: textChanger ? "100%" : 0,
+    animation: "typeAnimation 6s linear infinite",
+  };
+
+  return (
+    <div className="home-page-container">
+      <div className="home-page">
+        <div className="home-head-description-button">
+          <div className="header-text-home">
+            Hey there! I am Yeabsira,
+            <div className="job-description-animation" style={animStyle}>
+              {textChanger ? "AWS certified solution architect" : "Full stack developer"}
+            </div>
+          </div>
+          <p className="description-text-home">Hey, I'm Yeabsira, an AWS Certified Solution Architect and Full Stack Developer. Explore my portfolio to see my work</p>
+          <button className="contact-me-button-home-page">Contact me</button>
         </div>
-    )
+        <div className="home-image-and-effect"><img src={LandingPageImage} alt="cool-img"/></div>
+      </div>
+    </div>
+  );
 }
